dbaf55205ae1cc3c192da581b02aaff493159651
tuequach <tuequach@gmail.com>
2019-07-19 19:42:48 -0500

    changing json file name, initial states are all empty, and creaking an onclick event for when the image is clicked, it is being removed from array

--- one-punch-man/src/App.js
+++ one-punch-man/src/App.js
@@ -1,15 +1,22 @@
+//all dependencies and files are imported for use
 import React from 'react';
-
-
-
+import hero from './hero.json';
 
 class App extends Component {
+//setting all state to empty or 0
   state = {
     cards,
     score: "",
-    highscore: ""
+    clickedHero: []    
   };
 
+//creating onClick event for when image is clicked, a hero-card is taken out of the array
+onClick = event => {
+  const currentHero = event.target.alt;
+  const alreadyClickedHero = 
+    this.state.clickedHero.indexOf(currentHero) > -1;
+
+}
 
 
 export default App;
