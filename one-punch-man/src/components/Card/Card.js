64c274b090ad396c5b6dd0c4c3465cacd6fc0657
tuequach <tuequach@gmail.com>
2019-07-20 01:01:49 -0500

    rendering a card containing props passed that has id, key, image, along with header and footer

--- one-punch-man/src/components/Card/Card.js
+++ one-punch-man/src/components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./Card.css";
 
-function Card (props) {
+const Card = (props) => (
     <div className = 'card' onClick={props.imageClick}>
          <div className= 'img-container'>
             <img alt = {props.name} src={props.image} />
         </div>
     </div>
-}
+)
+
 export default Card;
--- one-punch-man/src/components/Header/Header.js
+++ one-punch-man/src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './header.css';
 
-function Header (props) {
+const Header = (props) => (
     <div className = 'header'>
         <div className = 'title'>One Punch Man!!!</div>
         <div className = 'scores'>
             Score: {props.score} Highscore: {props.highscore}
         </div>
     </div>
-};
+);
 
 export default Header;
