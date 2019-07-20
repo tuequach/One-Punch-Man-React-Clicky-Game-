7573743775f8442f485570cbba747801f30ed14c
tuequach <tuequach@gmail.com>
2019-07-20 09:59:37 -0500

    relocating files and folders into correct location for deployment

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return (
     <div className = 'header'>
-        <div className = 'title'>One Punch Man!!!</div>
+        <div className = 'title'>One Punch Man Memory Game!!!</div>
         <div className = 'scores'>
             Score: {this.props.score} Highscore: {this.props.highscore}
         </div>
